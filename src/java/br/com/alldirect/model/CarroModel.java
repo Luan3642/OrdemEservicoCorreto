0cf19fbf2744891fb6a389927b09646a292a66b9
Luan3642 <luan3642@hotmail.com>
2016-10-24 21:43:41 -0200

    Falta terminar

---
+++ src/java/br/com/alldirect/model/CarroModel.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.alldirect.model;
+
+/**
+ *
+ * @author luan
+ */
+public class CarroModel {
+    private String modeloCarro;
+    private String corDoCarro;
+    private int anoCarro;
+    private int QuantosCavalos;
+    
+}
